61ee1b16e1c2d838ddb22b5349681e1e84cb4544
dnzlucas <lucasdefenix@gmail.com>
2026-07-09 03:05:42 -0300

    feat: create default layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Fruits } from './pages/fruits/fruits';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: Home
+  },
+  {
+    path: "fruits",
+    component: Fruits
+  },
+  {
+    path: "about",
+    component: About
+  },
+  {
+    path: "contact",
+    component: Contact
+  }
+
+];
